f65bcdfaf0729a21bdfe49b5ba6e8400d1ec92f0
theo <theo.bonneville@ensae.fr>
2024-12-29 21:44:48 +0100

    Version casi final de l'analyse descriptive

--- regression_lineaire.py
+++ regression_lineaire.py
@@ -12,8 +12,7 @@ from sklearn.impute import SimpleImputer
 df = pd.read_excel('base_de_donnees_nettoyee.xlsx')
 
 print (df.info())
-# retirer les outliers
-# Liste des colonnes à vérifier
+# gerer les outliers des variables suivantes.
 colone_a_verifier = ['durée', 'prix', 'nominations', 'log_Box Office France']
 # Fonction pour retirer les outliers à l'aide de l'IQR
 def retirer_outliers(df, colones):
@@ -50,11 +49,11 @@ def winsorize(df,colone_a_verifier, borne_inferieur=5, borne_superieur=95):
     return df
 
 # Appliquer la fonction pour enlever les outliers
-#df_sans_outliers = retirer_outliers(df, colone_a_verifier)
+#df_sans_outliers = retirer_outliers(df, colone_a_verifier) # on ne retire pas les outliers directemmetn sinon on perd trop d'infos, on opte pour une winsorisation 
 df_sans_outliers = winsorize(df, colone_a_verifier) #on winsorize ces colones sinon on perd trop d'infos pres de20 % de la base de donnée. il serai taussi interessant d'introduirle champ log de budget mais le nombre de valeurs modifier est trop important pres de 60%
 
 print (df_sans_outliers.info())
-#effectuer un lasso sur notre base de données
+#effectuer un lasso sur notre base de données pour selectioner les varibles d'interet
 
 # Créer un objet imputer pour imputer les valeurs manquantes par la moyenne
 variables_interets=[ 'Presse', 'Spectateurs', 'durée', 'Année de production', 'nombre nationalités', 'prix', 'nominations', 'commercialiser', 'Comédie', 'Comédie dramatique', 'Drame', 'Aventure', 'Animation', 'Famille', 'Thriller', 'Action', 'Péplum', 'Historique', 'Fantastique', 'Comédie musicale', 'Romance', 'Epouvante-horreur', 'Biopic', 'Musical', 'Science Fiction', 'Guerre', 'Policier', 'Espionnage', 'Western', 'Erotique', 'Arts Martiaux', 'Judiciaire', 'Expérimental', 'Bollywood', 'Évènement Sportif', 'Drama', 'Divers', 'Concert', 'Spectacle', 'Opéra', 'nombre_actrice', 'pressence_realisatrice', 'log_Budget', 'log_Box Office France']
@@ -77,7 +76,7 @@ scaler = StandardScaler()
 v_explicatives_train_scaled = pd.DataFrame(scaler.fit_transform(v_explicatives_train),columns=variables_interets)
 v_explicatives_test_scaled = pd.DataFrame(scaler.transform(v_explicatives_test),columns=variables_interets)
 
-# Liste des valeurs alpha à tester
+# Liste des valeurs alpha à tester pour obtenir la meilleur regression en lasso
 valeurs_alpha = np.array([0.0001,0.0002,0.0005,0.001,0.002,0.05,0.01,0.02,0.025,0.05,0.1,0.25,0.5,0.8,1.0])
 
 # Créer le modèle Lasso
@@ -87,7 +86,7 @@ lasso = Lasso()
 param_grid = {'alpha': valeurs_alpha}
 
 # Utiliser GridSearchCV pour trouver le meilleur alpha
-grid_search = GridSearchCV(estimator=lasso, param_grid=param_grid, cv=5) # pour minimiser l'erreur quadratique moyenne (MSE)
+grid_search = GridSearchCV(estimator=lasso, param_grid=param_grid, cv=5)
 
 # Entraîner le modèle avec GridSearchCV
 grid_search.fit(v_explicatives_train_scaled, v_cible_train)
@@ -108,7 +107,7 @@ print(f"Mean Squared Error (MSE): {mse}")
 print(f"R² Score: {r2}")
 
 # Visualiser les coefficients associer a cette alpha optimal
-meilleur_lasso = grid_search.best_estimator_ # recuperer le lasso associé au meilleur alpha
+meilleur_lasso = grid_search.best_estimator_ # recuperer le lasso associer au meilleur alpha
 coefficients = meilleur_lasso.coef_# recuperer les coeficients
 coef = pd.DataFrame({
     "variables": v_explicatives_test_scaled.columns,  # Noms des colonnes
@@ -127,13 +126,13 @@ plt.show()
 
 # effectuer la regression linéaire sur les variables selectioner par le lasso
 
-# selectionnenr les variables selectionné par le lasso
+# selectionnenr les variables selectionner par le lasso
 v_explicatives_test_cut=v_explicatives_test[coef["variables"]]
 v_explicatives_train_cut=v_explicatives_train[coef["variables"]]
 
 # Ajouter une constante (intercept) à X
-v_explicatives_test_cut = sm.add_constant(v_explicatives_test_cut)  # Ajouter l'intercept 
-v_explicatives_train_cut = sm.add_constant(v_explicatives_train_cut)  # Ajouter l'intercept 
+v_explicatives_test_cut = sm.add_constant(v_explicatives_test_cut)  # Ajouter l'intercept à notre data frame de test
+v_explicatives_train_cut = sm.add_constant(v_explicatives_train_cut)  # Ajouter l'intercept à notre data frame d'entrainnement
 
 # Créer et entraîner le modèle OLS (Ordinary Least Squares)
 model = sm.OLS(v_cible_train,v_explicatives_train_cut)  # OLS : Ordinaire Moindres Carrés (Ordinary Least Squares)
@@ -153,3 +152,8 @@ r2 = r2_score(v_cible_test, y_pred)  # Coefficient de détermination R²
 print(f"Mean Squared Error (MSE) : {mse}")
 print(f"R² : {r2}")
 
+# En conclusion, même si les les coeficients , à l'exeption de ceux de genre, sont significatifs la faible valeur du R2 ,autour de 0.4, nous indique 
+# que notre regression n'est pas robuste. Peut étre que si on avait moins de valeurs manquantes (surtout concernant le budget), et l'acces à d'autres variables, comme à la qualités 
+# de l'image, ou le nombre d'image par secondes moyen,   notre resultat serait meilleur. 
+# Ou peut etre que le probléme repose sur le modéle chosit et qu'un modéle de classification aurait été plus adapté.
+#Neamoins dans l'état actuel notre regression est imparfaite
